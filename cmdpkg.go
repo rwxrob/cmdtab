7b6f11893fe615b4d2c4dd8fecfa9b1babb336a8
rwxrob <rwx@robs.io>
2020-11-28 21:07:59 -0500

    Added WaitForInterrupt.

--- cmdpkg.go
+++ cmdpkg.go
@@ -1,7 +1,10 @@
 package cmdtab
 
 import (
+	"context"
+	"log"
 	"os"
+	"os/signal"
 	"strings"
 	"syscall"
 )
@@ -179,4 +182,17 @@ func JSON() string {
 	s["Main"] = Main.Name
 	s["Commands"] = Visible()
 	return ConvertToJSON(s)
+}
+
+// WaitForInterrupt just blocks until an interrupt signal is received.
+// It should only be called from the main goroutine. It takes a single
+// context.CancelFunc that is designed to signal everything to stop
+// cleanly before exiting.
+func WaitForInterrupt(cancel context.CancelFunc) {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	defer cancel()
+	log.Println("waiting for interrupt")
+	<-interrupt
+	log.Println("received interrupt")
 }
