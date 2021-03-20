0b1b4b87698f931b2349e744152828142474fd9f
rwxrob <rob@rwx.gg>
2021-03-20 06:49:31 -0400

    Add User for mocks, etc. (close #16)

---
+++ user.go
@@ -0,0 +1,11 @@
+package cmdtab
+
+import "os/user"
+
+// User provides a more flexible way to deal with current user
+// information allowing applications to change the effective user ID
+// without using the original user and enabling mocks for tests that
+// involve user information.
+var User *user.User
+
+func init() { User, _ = user.Current() }
---
+++ user_test.go
@@ -0,0 +1,7 @@
+package cmdtab
+
+import "testing"
+
+func TestUser(t *testing.T) {
+	t.Log(User)
+}
